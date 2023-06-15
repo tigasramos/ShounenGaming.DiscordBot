7e47cfbdd97e707c99a78cd1c6fc4b5b9b687198
Tiago Ramos <tiago_bras_ramos@hotmail.com>
2023-06-15 17:29:14 +0100

    Logging , AppSettings & Discord Configuration

---
+++ ShounenGaming.DiscordBot/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShounenGaming.DiscordBot
+{
+    internal class AppSettings
+    {
+        public ServerSettings Server { get; set; }
+        public DiscordSettings Discord { get; set; }
+    }
+
+    internal class ServerSettings
+    {
+        public string Url { get; set; }
+        public string Port { get; set; }
+        public string BotDiscordId { get; set; }
+        public string BotPassword { get; set; }
+    }
+
+    internal class DiscordSettings
+    {
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+    }
+}
--- ShounenGaming.DiscordBot/Program.cs
+++ ShounenGaming.DiscordBot/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using ShounenGaming.DiscordBot;
+
+try
+{
+    //Configure Logger
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Console(outputTemplate: "[{Timestamp:dd/MM/yyyy - HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .WriteTo.File($"logs/log-.txt", rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:dd/MM/yyyy - HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .CreateLogger();
+
+    Log.Information("Starting ShounenGaming Discord Bot");
+
+    var services = CreateServiceProvider();
+    var appSettings = services.GetRequiredService<AppSettings>(); 
+    
+    //Start Bot
+    var discord = services.GetRequiredService<DiscordClient>();
+    await discord.ConnectAsync();
+
+
+    await Task.Delay(-1);
+} 
+catch(Exception ex)
+{
+    Log.Fatal(ex.Message);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static IServiceProvider CreateServiceProvider()
+{
+    var services = new ServiceCollection();
+
+    var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+            .AddJsonFile("appsettings.dev.json", optional: false, reloadOnChange: true)
+#else
+            .AddJsonFile("appsettings.prod.json", optional: false, reloadOnChange: true)
+#endif
+            .Build();
+
+    var appSettings = configuration.GetSection("settings").Get<AppSettings>();
+    services.AddSingleton<AppSettings>(_ => appSettings);
+    services.AddSingleton<DiscordClient>(_ => new DiscordClient(new DiscordConfiguration
+    {
+        Token = appSettings.Discord.Token,
+        TokenType = TokenType.Bot,
+        Intents = DiscordIntents.All,
+        LogTimestampFormat = "dd/MM/yyyy - HH:mm:ss"
+    }));
+
+    return services.BuildServiceProvider();
+}
